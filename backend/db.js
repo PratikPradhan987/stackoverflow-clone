7a6681f11806f45c43f738fa870c5758bbbdf00a
Pratik Pradhan <71559227+PratikPradhan987@users.noreply.github.com>
2022-08-04 17:23:48 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const url = 
-  // "mongodb://Pratik:pratik8250846027@cluster0-shard-00-00.rfatk.mongodb.net:27017,cluster0-shard-00-01.rfatk.mongodb.net:27017,cluster0-shard-00-02.rfatk.mongodb.net:27017/stackoverflow?ssl=true&replicaSet=atlas-i16i1b-shard-0&authSource=admin&retryWrites=true&w=majority";
-  "mongodb+srv://admin:admin123456789@cluster0.pgxcj.mongodb.net/?retryWrites=true&w=majority";
+
 module.exports.connect = () => {
   mongoose
-    .connect(url, {
+    .connect(process.env.url, {
       useNewUrlParser: true,
       // useFindAndModify: false,
       useUnifiedTopology: true,
